93664b078bc188f91215282b9bbec5a9988b47d4
lnkoenig <lkoenig4@gmail.com>
2021-07-04 11:03:56 -0500

    Update 03b_TextMining_FrequentWords.py
    Got the preprocess_string function to working (as much as possible)

--- 03b_TextMining_FrequentWords.py
+++ 03b_TextMining_FrequentWords.py
@@ -20,41 +20,50 @@ def preprocess_string(input): #makes lowercase, removes punctuation and common w
     if input==input:
         #import packages
         import nltk
-        nltk.download() #will slow things down if first time running
         from textblob import Word
         from nltk.corpus import stopwords
         import re
         
-        input=keyword_data['condition'][5]
-        print(input)
+        #input = keyword_data['brief_title'][1] #for testing
+        
         #make all letters lowercase, then remove punctuation and number
         input = re.sub(r"[^a-z;]"," ",input.lower())
 
         #remove excess spaces introduced by previous step
         input = " ".join(input.split())
-        
+        input = input.split(" ") #split again so can iterate over it
+
         #remove stop words
         stop = (stopwords.words('english'))
-        input = input.split(" ") 
+        input_no_stops = []
         for word in input:
-            #word_clean = str(TextBlob(word).correct()) #enable to fix spelling - not accurate in case of medical terms
-            word_clean = Word(word_clean).lemmatize() #remove endings
-            word_clean = word
+            if word not in stop:
+                input_no_stops.append(word) #delete string if a stop word
+        
+        #clean word
+        try:
+            for word in input_no_stops:
+                word_clean = word
+                #word_clean = str(TextBlob(word_clean).correct()) #enable to fix spelling - not accurate in case of medical terms
+                word_clean = Word(word_clean).lemmatize() #remove endings
+                input_no_stops[input_no_stops.index(word)] = str(word_clean) #replace word with clean version if not a stop word
+                
+        except Exception as ex:
+            print(ex)
+            print("It's likely you don't have nltk libraries installed, follow popup to install them")
+            nltk.download() #opens downloader          
 
-            if word_clean in stop:
-                input.remove(word) #delete string if a stop word
-            else:
-                input[input.index(word)] = str(word_clean) #replace word with clean version if not a stop word
+        #check for stop words again
+        input_no_stops2 = []
+        for word in input_no_stops:
+            if word not in stop:
+                input_no_stops2.append(word) #delete string if a stop word
+
+            
+        input_no_stops2 = " ".join(input_no_stops2) #turn back into a string
+        print(input_no_stops2)
         
-        #check stopwords twice
-        for word in input:
-              if word in stop:
-                input.remove(word) #delete string if a stop word
-                 
-        print(input)
-        input = " ".join(input) #turn back into a string
-        
-        return (input) 
+        return (input_no_stops2) 
 
 def remov_duplicates(input): #gets rid of duplicate words in string
     if input==input:
@@ -108,16 +117,16 @@ def remov_duplicates_semicolon(input): #gets rid of duplicate word chunks separa
                 break
 
         # joins two adjacent elements in iterable way 
-        s = " ".join(UniqW.keys()) #at this point there aren't any repeating words
+        s = ";".join(UniqW.keys()) #at this point there aren't any repeating words
         
         return (s)
 #%% process strings
 
-#makes lowercase, removes punctuation and common words, fixes spelling and lemmatization
-keyword_data = keyword_data.applymap(preprocess_string)
+#makes lowercase, removes punctuation and common words, lemmatization
+keyword_data2 = keyword_data.applymap(preprocess_string)
 
 #removes duplicates words within each record and feature
-keyword_data2 = keyword_data.applymap(remov_duplicates)
+keyword_data3 = keyword_data2.applymap(remov_duplicates)
 
 
 for i in keyword_data.columns:

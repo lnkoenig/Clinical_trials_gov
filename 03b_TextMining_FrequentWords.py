3736851a4390089f2e1af74547ff4ae0d8a9c6ac
Lauren Koenig <lkoenig4@gmail.com>
2021-07-09 22:02:34 -0500

    fixed n-gram counter so only considers n-grams within each record

--- 03b_TextMining_FrequentWords.py
+++ 03b_TextMining_FrequentWords.py
@@ -11,11 +11,12 @@ Created on Sat Jul  3 23:25:53 2021
 
 #import packages
 import pandas as pd 
+import numpy as np
 from collections import Counter
 from nltk import ngrams
 
 keyword_data = pd.read_csv('data-clean\keyword_data_clean.csv')
-keyword_data = keyword_data[0:40]
+#keyword_data = keyword_data[0:40]
 
 
 #%% take a look at the most common n-grams
@@ -23,45 +24,47 @@ keyword_data = keyword_data[0:40]
 for n_size in range(1,6):
     print("n size is ", n_size)
     for i in keyword_data.columns:
-        column_data = keyword_data[i].str.cat(sep=" ")   
-        ngram_counts = Counter(ngrams(column_data.split(" "), n_size))
+        ngram_counts = Counter()   # or nltk.FreqDist()
+        for sent in keyword_data[i]:     
+            if sent is not np.nan:
+                ngram_counts.update(ngrams(sent.split(" "), n_size))
         print(i)
-        print(ngram_counts.most_common(5))
+        print([" ".join(item) for item in list(next(zip(*ngram_counts.most_common(5))))])
+        
     
 #%% see how many ngrams occur semi frequently 
 
-for n_size in range(1,6):
+#how many ngrams occur in over 1% of cases?
+#looked at higher frequencies but get too few matches
+#1% also good to use because it matches the criteria used when cleaning the category_data
+ 
+frequency = 0.01
+#print out numbers
+for n_size in range(1,4):
     print("n size is ", n_size)
     for i in keyword_data.columns:
-        column_data = keyword_data[i].str.cat(sep=" ")   
-        ngram_counts = Counter(ngrams(column_data.split(" "), n_size))
+        ngram_counts = Counter()   # or nltk.FreqDist()
+        for sent in keyword_data[i]:     
+            if sent is not np.nan:
+                ngram_counts.update(ngrams(sent.split(" "), n_size))
         print(i)
-        print(len({x: count for x, count in ngram_counts.items() if count >= 0.01*len(keyword_data[i])})) #how many ngrams occur in over 1% of cases? - higher frequencies seem to be too few, and the 1% matches the criteria used in the category_data
-    
-  
-#%% see how many ngrams occur too frequently 
+        print(len({x: count for x, count in ngram_counts.items() if count >= frequency*len(keyword_data[i])})) 
 
-for n_size in range(1,6):
+#print out actual n-grams
+for n_size in range(1,4):
     print("n size is ", n_size)
     for i in keyword_data.columns:
-        column_data = keyword_data[i].str.cat(sep=" ")   
-        ngram_counts = Counter(ngrams(column_data.split(" "), n_size))
+        ngram_counts = Counter()   # or nltk.FreqDist()
+        for sent in keyword_data[i]:     
+            if sent is not np.nan:
+                ngram_counts.update(ngrams(sent.split(" "), n_size))
         print(i)
-        print(len({x: count for x, count in ngram_counts.items() if count >= 0.10*len(keyword_data[i])})) #how many ngrams occur in over 1% of cases? - higher frequencies seem to be too few, and the 1% matches the criteria used in the category_data
-
-#since few n-grams occur more than 10% of the time, I'm not going to worry about removing those that occur too frequently from the list
+        print({x: count for x, count in ngram_counts.items() if count >= frequency*len(keyword_data[i])})
         
 
 
-#%% make columns representing if the n-gram is found in a record 
+#%%  create columns representing if the n-gram is found in a record 
 
-for n_size in range(1,6):
-    for i in keyword_data.columns:
-        column_data = keyword_data[i].str.cat(sep=" ")   
-        ngram_counts = Counter(ngrams(column_data.split(" "), n_size))
-        print(i)
-        print({x: count for x, count in ngram_counts.items() if count >= 0.10*len(keyword_data[i])})
-        
 
 
 
